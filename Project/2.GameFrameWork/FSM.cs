199ef36f316f754ef764d39879cd5a1521dcd77d
梦小天幼 <2090311442@qq.com>
2023-09-27 00:23:30 +0800

    Create FSM.cs

---
+++ Project/2.GameFrameWork/FSM.cs
@@ -0,0 +1,35 @@
+@startuml
+
+
+namespace FSM {
+    
+    class Fsm
+    {
+        
+    }
+    class FsmBase 
+    {
+        有限状态机基类
+    }
+    class FsmState
+    {
+        状态基类
+    }
+    class FsmManager
+    {
+
+    }
+    
+
+    interface IFsm
+    {
+        有限状态机接口
+    }
+    interface IFsmManager
+    {
+        有限状态机管理器
+    }
+}
+
+
+@enduml
